c86a67651ef7a23d8795e4065675693f9753df3d
FelNavarroBrito <fel.navarrob@duocuc.cl>
2023-06-27 00:08:51 -0400

    limpiar

--- Entrega 3/apps/Tienda/static/js/mode-switcher.js
+++
@@ -1,6 +0,0 @@
-const modoOscuroBtn = document.getElementById('modo-oscuro-btn');
-const body = document.body;
-
-modoOscuroBtn.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-});
